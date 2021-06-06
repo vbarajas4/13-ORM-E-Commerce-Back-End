e3037b45cae8fe3bef358e55fe206dd108bf679d
Vanessa Maldonado <vbarajas4@gmail.com>
2021-06-05 22:48:54 -0700

    added model for ProductTag

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,20 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      //tbd reference
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      //reference
+    }
   },
   {
     sequelize,
